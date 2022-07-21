fc403cb7ab2f299131523d1aa9958b726151bfb5
prince_beladiya <84062079+PrinceBeladiya@users.noreply.github.com>
2022-07-21 15:10:00 +0530

    changes in index.js added

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
